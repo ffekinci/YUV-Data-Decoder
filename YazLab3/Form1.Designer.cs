89a8889d452ff807f37f468374055d3ad333a9af
Furkan Faruk Ekinci <ffekinci@gmail.com>
2018-12-15 19:45:27 +0300

    Add project files.

---
+++ YazLab3/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace YazLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.t_width = new System.Windows.Forms.TextBox();
+            this.t_height = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ok = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.type = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Height: ";
+            // 
+            // t_width
+            // 
+            this.t_width.Location = new System.Drawing.Point(67, 39);
+            this.t_width.Name = "t_width";
+            this.t_width.Size = new System.Drawing.Size(100, 20);
+            this.t_width.TabIndex = 1;
+            // 
+            // t_height
+            // 
+            this.t_height.Location = new System.Drawing.Point(67, 74);
+            this.t_height.Name = "t_height";
+            this.t_height.Size = new System.Drawing.Size(100, 20);
+            this.t_height.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "SEÇ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(67, 247);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 5;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "File:";
+            // 
+            // type
+            // 
+            this.type.FormattingEnabled = true;
+            this.type.Items.AddRange(new object[] {
+            "4:4:4",
+            "4:2:2",
+            "4:2:0"});
+            this.type.Location = new System.Drawing.Point(67, 193);
+            this.type.Name = "type";
+            this.type.Size = new System.Drawing.Size(100, 21);
+            this.type.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Dosya: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Type:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(208, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(580, 352);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "PLAY";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.type);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.t_height);
+            this.Controls.Add(this.t_width);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "YazLabIII - YUV Decoder";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox t_width;
+        private System.Windows.Forms.TextBox t_height;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox type;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ YazLab3/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazLab3
+{
+    public partial class Form1 : Form
+    {
+        String path;
+        int width, height;
+        int total_frame;
+        int TimerCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "yuv files (*.yuv)|*.yuv";
+            dialog.InitialDirectory = @"C:\Users\ffekinci\Desktop";
+            dialog.Title = "Please select an image file to encrypt.";
+
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                path = dialog.FileName;
+                Console.WriteLine(path);
+            }
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            string a = t_height.Text;
+            int result;
+            if (!int.TryParse(a, out result))
+            {
+                MessageBox.Show("Lütfen geçerli değer girin!");
+                return;
+            }
+            height = result;
+
+            result = 0;
+            string b = t_width.Text;
+            if (!int.TryParse(b, out result))
+            {
+                MessageBox.Show("Lütfen geçerli değer girin!");
+            }
+            width = result;
+
+            var v = type.SelectedIndex;
+            Console.WriteLine(v);
+
+            if (v == 0)
+            {
+                Decode444(path);
+                ClearFolder();
+            }
+            else if (v == 1)
+            {
+                Decode422(path);
+                ClearFolder();
+            }
+            else if (v == 2)
+            {
+                ClearFolder();
+                Decode420(path);
+            }
+        }
+
+        private void Decode444(string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            //Console.WriteLine(bytes.Length);
+
+            total_frame = bytes.Length / (width * height * 3);
+            //trackBar1.Minimum = 1;
+            //trackBar1.Maximum = total_frame;
+            //frame.Text = "Frame : 1";
+
+            Bitmap bitmap = new Bitmap(width, height);
+            Color color;
+
+            for (int m = 0; m < total_frame; m++)
+            {
+
+                int j = 0, k = 0;
+                int begin = m * width * height * 3;
+                for (int i = begin; i < begin + width * height; i++)
+                {
+                    color = Color.FromArgb(bytes[i], bytes[i], bytes[i]);
+
+                    bitmap.SetPixel(k, j, color);
+
+                    k++;
+
+                    if (k == width)
+                    {
+                        k = 0;
+                        j++;
+                    }
+                }
+                string s = "frame-" + m + ".bmp";
+                pictureBox1.Image = bitmap;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                bitmap.Save("bmp/"+s);
+
+            }
+
+
+        }
+
+        private void Decode422(string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            //Console.WriteLine(bytes.Length);
+
+            total_frame = bytes.Length / (width * height * 2);
+            //trackBar1.Minimum = 1;
+            //trackBar1.Maximum = total_frame;
+            //frame.Text = "Frame : 1";
+
+            Bitmap bitmap = new Bitmap(width, height);
+            Color color;
+
+            for (int m = 0; m < total_frame; m++)
+            {
+
+                int j = 0, k = 0;
+                int begin = m * width * height * 2;
+                for (int i = begin; i < begin + width * height; i++)
+                {
+                    color = Color.FromArgb(bytes[i], bytes[i], bytes[i]);
+
+                    bitmap.SetPixel(k, j, color);
+
+                    k++;
+
+                    if (k == width)
+                    {
+                        k = 0;
+                        j++;
+                    }
+                }
+                string s = "frame-" + m + ".bmp";
+                pictureBox1.Image = bitmap;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                bitmap.Save("bmp/"+s);
+
+            }
+
+
+        }
+
+        private void Decode420(string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            //Console.WriteLine(bytes.Length);
+
+            total_frame = (int) (bytes.Length / (width * height * 1.5));
+            //trackBar1.Minimum = 1;
+            //trackBar1.Maximum = total_frame;
+            //frame.Text = "Frame : 1";
+
+            Bitmap bitmap = new Bitmap(width, height);
+            Color color;
+
+            for (int m = 0; m < total_frame; m++)
+            {
+
+                int j = 0, k = 0;
+                int begin =(int)( m * width * height * 1.5);
+                for (int i = begin; i < begin + width * height; i++)
+                {
+                    color = Color.FromArgb(bytes[i], bytes[i], bytes[i]);
+
+                    bitmap.SetPixel(k, j, color);
+
+                    k++;
+
+                    if (k == width)
+                    {
+                        k = 0;
+                        j++;
+                    }
+                }
+                string s = "frame-" + m + ".bmp";
+                pictureBox1.Image = bitmap;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                bitmap.Save("bmp/"+s);
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            timer1.Interval = 40;
+            timer1.Enabled = true;
+            pictureBox1.Image = new Bitmap("bmp/frame-1.bmp");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimerCount++;
+            if(TimerCount == total_frame-1)
+            {
+                timer1.Enabled = false;
+                
+            }
+            pictureBox1.Image = new Bitmap("bmp/frame-" + TimerCount + ".bmp");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        public void ClearFolder()
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo("bmp");
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+
+
+
+    }
+}
